75898d5503e98d327c21e4834cc4cb651f96a5b1
tsy <996929281@qq.com>
2022-11-04 10:36:35 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# To install using pip
+* download docxtpl_dynamic_vertical_merging.zip && pip3 install docxtpl_dynamic_vertical_merging.zip
+* pip3 install git+https://github.com/tsy19900929/docxtpl_dynamic_vertical_merging.git
+
+# Usage
+* test1
+* test2
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='docxtpldvm',
+      version='0.1',
+      description='Enable dynamic vertical merging when use python-docx-template.',
+      url='https://github.com/tsy19900929/docxtpl_dynamic_vertical_merging',
+      author='tsy',
+      author_email='tsy19900929@gmail.com',
+      license='LGPL 2.1',
+      packages=['docxtpldvm'],
+      install_requires=[],
+      zip_safe=False)
